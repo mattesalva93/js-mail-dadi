f81acbc53af2d84660a99d75d036f204cbed7b21
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-13 16:23:19 +0100

    mail done

--- mail/myscript.js
+++ mail/myscript.js
@@ -1,7 +1,9 @@
 //creare un array che definisca un elenco di mail
 const utenti = ["tizioincognito@segreto.it", "tiziosospetto@segreto.it", "tiziosegreto@segreto.it", "tizionascosto@segreto.it", "tizioocculto@segreto.it", "tiziomalandrino@segreto.it"];
+
 //richiede all'utente di inserire la propria mail
 const emailinput = prompt("Inserisci la tua email")
+
 //creare ciclo che definisca in caso di esito positivo o negativo messaggio
 for (let i = 0; i < utenti.length; i++) {
     if (utenti[i] == emailinput) {
