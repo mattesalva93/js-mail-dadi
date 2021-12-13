f301b86c4a74fac983a7ac39370fee721a296b89
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-13 21:54:35 +0100

    mail done without prompt

--- mail/myscript.js
+++ mail/myscript.js
@@ -1,19 +1,26 @@
 //creare un array che definisca un elenco di mail
 const utenti = ["tizioincognito@segreto.it", "tiziosospetto@segreto.it", "tiziosegreto@segreto.it", "tizionascosto@segreto.it", "tizioocculto@segreto.it", "tiziomalandrino@segreto.it"];
 
-//richiede all'utente di inserire la propria mail
-const emailinput = prompt("Inserisci la tua email")
 
 //creare ciclo che definisca in caso di esito positivo o negativo messaggio
-for (let i = 0; i < utenti.length; i++) {
-    if (utenti[i] == emailinput) {
-        console.log("Email corretta");
-        scatola.innerHTML += '<div class="col- m-1 text-center">' + 'Benvenuto '  + emailinput + '</div>';
-        break
+let emailcheck = document.querySelector(".btn");
+
+emailcheck.addEventListener("click", function(){
+    
+    //richiede all'utente di inserire la propria mail
+    let emailinput = document.getElementById("emailinput").value;
+    console.log(emailinput);
+    
+    for (let i = 0; i < utenti.length; i++) {
+        if (utenti[i] != emailinput) {
+            console.log("Email errata");
+            scatola.innerHTML = '<div class="col- m-1 text-center"> Non c\'è nulla da vedere qui, circolare </div>';
+
+        }else{
+            console.log("Email corretta");
+            scatola.innerHTML = '<div class="col- m-1 text-center">' + 'Benvenuto '  + emailinput + '</div>';
+            break
+        }
     }
-    else{
-        console.log("Email errata");
-        scatola.innerHTML += '<div class="col- m-1 text-center"> Non c\'è nulla da vedere qui, circolare </div>';
-        break
-    }
-}
+
+})
