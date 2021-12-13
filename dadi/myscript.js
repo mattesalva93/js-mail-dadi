e8bb836d8b68f0aab3ab941e3f93dbb1fbc7d721
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-13 18:55:07 +0100

    visual dice outcome done

--- dadi/myscript.js
+++ dadi/myscript.js
@@ -14,3 +14,35 @@ if (tiroutente > tiropc){
 }else{
     console.log("Pareggio!");
 }
+
+//if per possibile comparse del dado dell'utente
+let scatola = document.getElementById("dadoutente");
+if(tiroutente == 1){
+    scatola.innerHTML += '<img src="img/dice-six-faces-one.svg">';
+}else if (tiroutente == 2){
+    scatola.innerHTML += '<img src="img/dice-six-faces-two.svg">';
+}else if (tiroutente == 3){
+    scatola.innerHTML += '<img src="img/dice-six-faces-three.svg">';
+}else if (tiroutente == 4){
+    scatola.innerHTML += '<img src="img/dice-six-faces-four.svg">';
+}else if (tiroutente == 5){
+    scatola.innerHTML += '<img src="img/dice-six-faces-five.svg">';
+}else{
+    scatola.innerHTML += '<img src="img/dice-six-faces-six.svg">';
+}
+
+//if per possibile comparse del dado del pc
+let scatola2 = document.getElementById("dadopc");
+if(tiropc == 1){
+    scatola2.innerHTML += '<img src="img/inverted-dice-1.svg">';
+}else if (tiropc == 2){
+    scatola2.innerHTML += '<img src="img/inverted-dice-2.svg">';
+}else if (tiropc == 3){
+    scatola2.innerHTML += '<img src="img/inverted-dice-3.svg">';
+}else if (tiropc == 4){
+    scatola2.innerHTML += '<img src="img/inverted-dice-4.svg">';
+}else if (tiropc == 5){
+    scatola2.innerHTML += '<img src="img/inverted-dice-5.svg">';
+}else{
+    scatola2.innerHTML += '<img src="img/inverted-dice-6.svg">';
+}
