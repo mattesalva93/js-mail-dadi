b63bb1493133df8ffb44e0ad0310b32015a2c3b4
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-14 10:29:17 +0100

    post correzione classe

--- dadi/myscript.js
+++ dadi/myscript.js
@@ -13,50 +13,43 @@ tiro.addEventListener("click", function(){
     console.log(tiropc);
 
 
-//condizioni per decretare vittoria/pareggio/sconfitta
-let esito = document.getElementById("esito");
-if (tiroutente > tiropc){
-    console.log("Hai vinto!");
-    esito.innerHTML = 'TU! HAI VINTO!'
-}else if (tiroutente < tiropc){
-    console.log("Hai perso!");
-    esito.innerHTML = 'TU! HAI PERSO!'
-}else{
-    console.log("Pareggio!");
-    esito.innerHTML = 'PAREGGIO!'
-}
+    //condizioni per decretare vittoria/pareggio/sconfitta
+    let esito = document.getElementById("esito");
+    if (tiroutente > tiropc){
+        console.log("Hai vinto!");
+        esito.innerHTML = 'TU! HAI VINTO!'
+    }else if (tiroutente < tiropc){
+        console.log("Hai perso!");
+        esito.innerHTML = 'TU! HAI PERSO!'
+    }else{
+        console.log("Pareggio!");
+        esito.innerHTML = 'PAREGGIO!'
+    }
 
+    //if per possibile comparse del dado dell'utente
+    let scatola = document.getElementById("dadoutente");
+    switch(tiroutente){
+        case 1:
+            scatola.innerHTML = '<img src="img/dice-six-faces-one.svg">';
+            break;
+        case 2:
+            scatola.innerHTML = '<img src="img/dice-six-faces-two.svg">';
+            break;
+        case 3:
+            scatola.innerHTML = '<img src="img/dice-six-faces-three.svg">';
+            break;
+        case 4:
+            scatola.innerHTML = '<img src="img/dice-six-faces-four.svg">';
+            break;
+        case 5:
+            scatola.innerHTML = '<img src="img/dice-six-faces-five.svg">';
+            break;
+        case 6:
+            scatola.innerHTML = '<img src="img/dice-six-faces-six.svg">';
+            break;
+    }
 
-//if per possibile comparse del dado dell'utente
-let scatola = document.getElementById("dadoutente");
-if(tiroutente == 1){
-    scatola.innerHTML = '<img src="img/dice-six-faces-one.svg">';
-}else if (tiroutente == 2){
-    scatola.innerHTML = '<img src="img/dice-six-faces-two.svg">';
-}else if (tiroutente == 3){
-    scatola.innerHTML = '<img src="img/dice-six-faces-three.svg">';
-}else if (tiroutente == 4){
-    scatola.innerHTML = '<img src="img/dice-six-faces-four.svg">';
-}else if (tiroutente == 5){
-    scatola.innerHTML = '<img src="img/dice-six-faces-five.svg">';
-}else{
-    scatola.innerHTML = '<img src="img/dice-six-faces-six.svg">';
-}
-
-//if per possibile comparse del dado del pc
-let scatola2 = document.getElementById("dadopc");
-if(tiropc == 1){
-    scatola2.innerHTML = '<img src="img/inverted-dice-1.svg">';
-}else if (tiropc == 2){
-    scatola2.innerHTML = '<img src="img/inverted-dice-2.svg">';
-}else if (tiropc == 3){
-    scatola2.innerHTML = '<img src="img/inverted-dice-3.svg">';
-}else if (tiropc == 4){
-    scatola2.innerHTML = '<img src="img/inverted-dice-4.svg">';
-}else if (tiropc == 5){
-    scatola2.innerHTML = '<img src="img/inverted-dice-5.svg">';
-}else{
-    scatola2.innerHTML = '<img src="img/inverted-dice-6.svg">';
-}
-
+    //if per possibile comparse del dado del pc
+    let scatola2 = document.getElementById("dadopc");
+    scatola2.innerHTML = '<img src="img/inverted-dice-'+tiropc+'.svg">';
 })
